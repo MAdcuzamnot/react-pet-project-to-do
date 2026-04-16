f1db1dffcd30af782c8d892c641a5f3c8bd1ac96
MAdcuzamnot <armednov@gmail.com>
2026-04-16 19:00:32 +0300

    start

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# react-pet-project-to-do
-A personal React project that will include a to-do list
+hello :)
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>To Do list</h1>
+    </div>
+  )
+}
+
+
+export default App
